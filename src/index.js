c7b17449c47c00d00bf67261b9e1a62cd47d8412
Key <54068965+bhn0108@users.noreply.github.com>
2022-04-10 22:58:55 +0900

    add_complete-button

--- src/index.js
+++ src/index.js
@@ -17,18 +17,41 @@ const onClickAdd = () => {
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
   completeButton.addEventListener("click", () => {
-    alert("完了");
+    //押された削除ボタンの親(div)を未完了リストから削除する
+    deleteFromIncompleteList(completeButton.parentNode);
+
+    //押された完了ボタンの親(div)を完了リストに移動する
+    //完了リストに追加する要素
+    const addTarget = completeButton.parentNode;
+
+    //TODO内容を取得する
+    const text = addTarget.firstChild.innerText;
+
+    //addTargetを初期化する
+    addTarget.textContent = null;
+
+    //liを生成する
+    const li = document.createElement("li");
+    li.innerText = text;
+
+    //buttonを生成する
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+
+    //divの下に要素を生成する
+    addTarget.appendChild(li);
+    addTarget.appendChild(backButton);
+
+    //未完了のリストに追加する
+    document.getElementById("complete-list").appendChild(addTarget);
   });
 
   //button(削除)を生成する
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
   deleteButton.addEventListener("click", () => {
-    alert("削除");
-
     //押された削除ボタンの親(div)を未完了リストから削除する
-    const deleteTarget = deleteButton.parentNode;
-    document.getElementById("incomplete-list").removeChild(deleteTarget);
+    deleteFromIncompleteList(deleteButton.parentNode);
   });
 
   //divの下にpを生成する
@@ -38,6 +61,11 @@ const onClickAdd = () => {
 
   //未完了のリストに追加する
   document.getElementById("incomplete-list").appendChild(div);
+};
+
+//未完了リストから指定の要素を削除
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
 };
 
 document
